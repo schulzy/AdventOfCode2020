a71064f8a079e0dd95f3160cf4949243097e523d
schulzy <schulzyy@gmail.com>
2020-12-09 23:00:43 +0100

    Init Day07, Day08, Day09

---
+++ AdventOfCode2020/Day07/Day07Module.cs
@@ -0,0 +1,20 @@
+﻿using Day07.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Day07
+{
+    public class Day07Module : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ AdventOfCode2020/Day09/Day09Module.cs
@@ -0,0 +1,20 @@
+﻿using Day09.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Day09
+{
+    public class Day09Module : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
